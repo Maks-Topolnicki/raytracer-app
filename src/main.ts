c5bddacb95dbee508fe74ff0c90218934c77d892
Maks Topolnicki <makstopolnicki@gmail.com>
2026-07-11 20:53:17 +0200

    ImageData Optimalization

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,34 @@ const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 const sphere = new Sphere(new Vec3(0, 0, -3), 1);
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 const center = new Vec3(0, 0, -3);
 const radius = 1;
 
+const imageData = ctx.createImageData(canvas.width, canvas.height);
+const data = imageData.data;
+
 for (let py = 0; py < 600; py++) {
   for (let px = 0; px < 800; px++) {
-    let x = (px - 400) / 400;
+    let x = (px - 400) / 300;
     let y = ((py - 300) * -1) / 300;
 
     const direction = new Vec3(x, y, -1).normalize();
     const ray = new Ray(new Vec3(0, 0, 0), direction);
 
+    const index = (py * canvas.width + px) * 4;
+
     if (sphere.intersect(ray) !== null) {
-      ctx.fillStyle = "red";
-      ctx.fillRect(px, py, 1, 1);
+      data[index] = 255; // R
+      data[index + 1] = 0; // G
+      data[index + 2] = 0; // B
+      data[index + 3] = 255; // A (255 - w pełni widoczne)
+    } else {
+      data[index] = 0; // R
+      data[index + 1] = 0; // G
+      data[index + 2] = 0; // B
+      data[index + 3] = 255; // A (255 - w pełni widoczne)
     }
   }
 }
+
+ctx.putImageData(imageData, 0, 0);
