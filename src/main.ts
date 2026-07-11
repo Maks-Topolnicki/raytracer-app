147a716afd5d99c42371b7962805cc9c5d7653d9
Maks Topolnicki <makstopolnicki@gmail.com>
2026-07-11 21:51:40 +0200

    Shading

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ const sphere = new Sphere(new Vec3(0, 0, -3), 1);
 
 const center = new Vec3(0, 0, -3);
 const radius = 1;
+const light = new Vec3(2, 2, 0);
 
 const imageData = ctx.createImageData(canvas.width, canvas.height);
 const data = imageData.data;
@@ -19,11 +20,16 @@ for (let py = 0; py < 600; py++) {
 
     const direction = new Vec3(x, y, -1).normalize();
     const ray = new Ray(new Vec3(0, 0, 0), direction);
-
     const index = (py * canvas.width + px) * 4;
+    const t = sphere.intersect(ray);
 
-    if (sphere.intersect(ray) !== null) {
-      data[index] = 255; // R
+    if (t !== null) {
+      const P = ray.pointAt(t);
+      const normal = P.sub(sphere.center).normalize();
+      const lightDir = light.sub(P).normalize();
+      const brightness = Math.max(0, normal.dot(lightDir));
+
+      data[index] = 255 * brightness; // R
       data[index + 1] = 0; // G
       data[index + 2] = 0; // B
       data[index + 3] = 255; // A (255 - w pełni widoczne)
